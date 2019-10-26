686bf65b8da7da020a9c08d5e0b8df81aa5ada1b
David <dfain@wemaintain.io>
2019-10-26 17:39:31 +0200

    new polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
+## Add this to your wm startup file.
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar screen & polybar wide
-
-
-echo "Bars launched..."
+# Launch Polybar
+polybar -c ~/.config/polybar/config.ini top &
+polybar -c ~/.config/polybar/config.ini bottom &
+/usr/bin/env python3 ~/.config/polybar/scripts/spotify/py_spotify_listener.py &
